c93a26d3ef03f68d05fd65a4470cd96008ffb489
Philippe~ <philippe.lequoc@gmail.com>
2023-04-11 14:06:37 +0200

    modif connection + route post

--- backend/model/connection.js
+++ backend/model/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const mongodb_url =
-  "mongodb+srv://philippelacapsule:H1r0nd3773@cluster0.6dvqohn.mongodb.net/test";
+// const mongodb_url =
+//   "mongodb+srv://philippelacapsule:H1r0nd3773@cluster0.6dvqohn.mongodb.net/test";
 const db_name = "TicketHack";
-const connectionString = `mongodb+srv://didier:.didier@cluster1.ffivh9w.mongodb.net/${db_name}`;
+const connectionString = `mongodb+srv://philippelacapsule:H1r0nd3773@cluster0.6dvqohn.mongodb.net/${db_name}`;
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
--- backend/routes/trip.js
+++ backend/routes/trip.js
@@ -1,11 +1,18 @@
 var express = require("express");
 var router = express.Router();
-
+const Trip = require("../model/trip");
 require("../model/connection");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+// recherche trip par departure, arrival et date
+router.post("/", function (req, res) {
+  const startDate = new Date(req.body.date);
+  const endDate = new Date(startDate.getTime() + 86400000);
+  Trip.find({
+    departure: { $regex: new RegExp(req.body.departure, "i") },
+    arrival: { $regex: new RegExp(req.body.arrival, "i") },
+    date: { $gte: startDate, $lt: endDate },
+  }).then((data) => res.json({ data }));
+  
 });
 
 module.exports = router;
